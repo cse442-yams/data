df67da3c2099fd6cd5d8112f68e2faeb410920a4
Alex Kofke <akofke@gmail.com>
2018-10-14 23:44:04 -0400

    Work on inserting game data

--- db_insert.py
+++ db_insert.py
@@ -2,7 +2,7 @@ import psycopg2 as pg
 import json
 import csv
 from collections import defaultdict
-
+from datetime import datetime
 
 def get_conn():
     return pg.connect(database="cse442", user="alex", host="localhost")
@@ -36,15 +36,57 @@ def insert_games():
     with open("nba-historical/nba_games_all.csv") as f, get_conn() as conn:
         reader = csv.DictReader(f)
         for row in reader:
-            is_home = row["is_home"] == "t"
-            team_games[row['game_id']]['home' if is_home else 'away'] = row
+            if row['w'] != "" and row["l"] != "" and row["game_date"] != "" and row["wl"] != "":
+                is_home = row["is_home"] == "t"
+                team_games[row['game_id']]['home' if is_home else 'away'] = row
 
+    team_game_stmt = """
+    INSERT INTO basketball.nba_team_games (team_id, is_home, result, current_wins, current_losses, fgm, fga, fg3m, fg3a, ftm, fta, oreb, dreb, reb, ast, stl, blk, tov, pf, pts)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    RETURNING id
+    """
+
+    game_stmt = """
+    INSERT INTO basketball.nba_games (original_id, game_date, season_type, home_team_game_id, away_team_game_id, minutes)
+    VALUES (%s, %s, %s, %s, %s, %s)
+    """
+
+    for id, g in team_games.items():
+        if g['home']['team_id'] not in csv_id_to_db_id or g['away']['team_id'] not in csv_id_to_db_id:
+            continue
+        with conn.cursor() as cur:
+            cur.execute(team_game_stmt, _team_game_tuple(g['home'], csv_id_to_db_id))
+            home_id = cur.fetchone()[0]
+
+        with conn.cursor() as cur:
+            cur.execute(team_game_stmt, _team_game_tuple(g['away'], csv_id_to_db_id))
+            away_id = cur.fetchone()[0]
+
+        h = g['home']
+        game_tuple = (h['game_id'], datetime.strptime(h['game_date'], "%Y-%d-%M"), h['season_type'], home_id, away_id, h['min'])
+        with conn.cursor() as cur:
+            cur.execute(game_stmt, game_tuple)
+
+
+
+def _team_game_tuple(r, team_ids):
+    t = (team_ids[r['team_id']], r['is_home'], game_result(r['wl']), r['w'], r['l'], r['fgm'], r['fga'], r['fg3m'], r['fg3a'], r['ftm'], r['fta'], r['oreb'], r['dreb'], r['reb'], r['ast'], r['stl'], r['blk'], r['tov'], r['pf'], r['pts'])
+    return t
+
+def game_result(s):
+    if s == "W":
+        return "Win"
+    if s == "L":
+        return "Loss"
+    else:
+        raise ValueError
 
 
 
 
 def main():
-    insert_teams()
+    # insert_teams()
+    insert_games()
 
 if __name__ == '__main__':
     main()
